ddf56efc98d180d0718792a190de73c497208355
taegyun <needle12s@gmail.com>
2026-01-28 00:14:23 +0900

    Math Solved - A + B

--- baekjoon/Sprout/1000_A+B/Math_SOLVED/P1000_v2.java
+++ baekjoon/Sprout/1000_A+B/Math_SOLVED/P1000_v2.java
@@ -5,12 +5,15 @@ import java.util.StringTokenizer;
 
 public class P1000_v2 {
 
+    static int A;
+    static int B;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
 
         System.out.println(A + B);
     }
