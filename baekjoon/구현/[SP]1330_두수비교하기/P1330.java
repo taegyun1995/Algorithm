78ad6a6133bb4d29ea15a37131df81b606031fb0
taegyun <needle12s@gmail.com>
2026-02-16 14:42:19 +0900

    [구현/새싹]두 수 비교하기

---
+++ baekjoon/구현/[SP]1330_두수비교하기/P1330.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P1330 {
+    static int A;
+    static int B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A == B) {
+            System.out.println("==");
+        } else {
+            System.out.println("<");
+        }
+    }
+}
