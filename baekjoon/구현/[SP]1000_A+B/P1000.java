664024c20fb325a857e57b125fd3ff803ea61860
taegyun <needle12s@gmail.com>
2026-02-16 14:24:18 +0900

    [구현/새싹]A + B

---
+++ baekjoon/구현/[SP]1000_A+B/P1000.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P1000 {
+    static int A;
+    static int B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A + B);
+    }
+}
